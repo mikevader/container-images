87754e0485ac1a1802259245e7a9470f4538b43b
Michael Mühlebach <michael@anduin.ch>
2022-08-30 23:29:20 +0200

    Add ubuntu base image

---
+++ base/ubuntu-jammy/shim/greeting.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "
+Welcome to a mikevader's container,
+You are entering the vicinity of an area adjacent to a location.
+The kind of place where there might be a monster, or some kind of weird mirror.
+These are just examples; it could also be something much better.
+* Repository: https://github.com/mikevader/container-images
+* Docs: https://mikevader.github.io/k3s-cluster-infra-apps/
+* Bugs or feature requests should be opened in an GH issue
+* Questions should be discussed in Discord or GH discussions
+"
