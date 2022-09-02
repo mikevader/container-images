ab7548202ded05a233004641ba6675aa8feaa30f
Michael Mühlebach <michael@anduin.ch>
2022-09-02 19:01:16 +0200

    Make dockerfile work

--- apps/airprint/entrypoint.sh
+++ apps/airprint/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 #shellcheck disable=SC1091
-source "/shim/umask.sh"
+#source "/shim/umask.sh"
 
 export APACHE_RUN_DIR=/app
 export APACHE_LOG_DIR=/app
 export APACHE_PID_FILE=/tmp/apache.pid
 export APACHE_RUN_USER=lucy
 export APACHE_RUN_GROUP=lucy
+
+touch /app/access.log
+touch /app/error.log
 
 ln -sf /dev/stdout /app/access.log \
 ln -sf /dev/stderr /app/error.log
