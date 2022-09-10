30c141abb2eca037aa60fbfa49f42a4489b0e8bc
Michael Mühlebach <michael@anduin.ch>
2022-09-10 15:08:49 +0200

    Add error responder

--- apps/rails-demo/app/controllers/pages_controller.rb
+++ apps/rails-demo/app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
     def home
+        if params[:status] == "error"
+            if params[:code] != nil
+                head params[:code]
+            else
+                head 500
+            end
+        end
     end
 end
