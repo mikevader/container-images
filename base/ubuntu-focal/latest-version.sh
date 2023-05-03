c6046fc247d4fd94175985b5ba324bdf875df923
Michael Mühlebach <michael@anduin.ch>
2023-05-03 23:00:49 +0200

    Fix version issue of focal base image

--- base/ubuntu-focal/latest-version.sh
+++ base/ubuntu-focal/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags" | jq --raw-output '.results[] | select(.name | contains("focal")) | .name' | sort | tail -n1)
+version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags?ordering=name&name=focal" | jq --raw-output '.results[] | select(.name | contains("focal")) | .name' | head -n1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
