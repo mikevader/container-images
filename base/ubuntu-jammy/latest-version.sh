ba7475ab17de818b23e9c0db5c180035fcbad197
Michael Mühlebach <michael@anduin.ch>
2023-05-03 19:25:45 +0200

    Fix jammy update script

--- base/ubuntu-jammy/latest-version.sh
+++ base/ubuntu-jammy/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/ubuntu/tags" | jq --raw-output '.[] | select(.name | contains("jammy")) | .name'  | tail -n1)
+version=$(curl -s "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags" | jq --raw-output '.results[] | select(.name | contains("jammy")) | .name' | sort | tail -n1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
